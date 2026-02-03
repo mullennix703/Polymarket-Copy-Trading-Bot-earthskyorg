7bcb151ad710b7f8a7dbfd3ae7204cccb1adb9d6
mullennix703 <68289038+mullennix703@users.noreply.github.com>
2026-02-03 22:18:36 +0800

    Add ForesightOracle to Politics category

--- TypeScript/src/config/env.ts
+++ TypeScript/src/config/env.ts
@@ -24,6 +24,7 @@ const KNOWN_TRADERS_INFO: TraderInfo[] = [
     { address: '0xdfe3fedc5c7679be42c3d393e99d4b55247b73c4', name: 'cqs', category: 'Politics' },
     { address: '0xa4b366ad22fc0d06f1e934ff468e8922431a87b8', name: 'HolyMoses7', category: 'Politics' },
     { address: '0x2785e7022dc20757108204b13c08cea8613b70ae', name: 'poorsob', category: 'Politics' },
+    { address: '0x7072dd52161bae614bec6905846a53c9a3a53413', name: 'ForesightOracle', category: 'Politics' },
     // { address: '0x8545ff3521691618f2d5e4f5460d76186a5023be', name: '1KChallenge' },
     { address: '0x751a2b86cab503496efd325c8344e10159349ea1', name: 'Sharky6999', category: 'UpDown' },
     { address: '0xc747b8f900a08f59974f50989e0703344ba0324b', name: 'Nadin592', category: 'UpDown' },
